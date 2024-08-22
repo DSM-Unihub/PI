b4aec251311f7f1576ddb5d6ab15535475a90a13
Daniel Augusto Mandira <daniel.mandira@hotmail.com>
2024-08-21 22:57:32 -0300

    Inicio da Transformação do index do EJS em Next
    Corrigir a função de graficos do canvas

--- DW3/Site/resist/src/app/layout.js
+++ DW3/Site/resist/src/app/layout.js
@@ -12,6 +12,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
       <body className={inter.className}>{children}</body>
+      <script src="js/script.js"></script>
+      <script src="https://code.highcharts.com/dashboards/dashboards.js"></script>
+      <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+      <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@0.7.0"></script>
     </html>
   );
 }
--- DW3/Site/resist/src/app/page.js
+++ DW3/Site/resist/src/app/page.js
@@ -1,10 +1,15 @@
 import Image from "next/image";
-import Header from "@/components/header";
+import Header from "@/components/partials/headerBar";
+import NavBar from "@/components/partials/navBar";
 export default function Home() {
   return (
     <main>
-      <Header/>
+      <section class="flex flex-row justify-start h-screen bg-gradient-to-tr from-cinza-secundario to-cinza-principal w-full h-full">
+          <NavBar />
+        <section class="flex flex-col w-full lg:overflow-hidden">
+              <Header />
+        </section>
+      </section>
     </main>
-      
   );
 }
