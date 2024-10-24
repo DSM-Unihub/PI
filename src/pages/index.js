0507b11f0c329d2497e6132adb8ef84d5e6271a0
Daniel Augusto Mandira <daniel.mandira@hotmail.com>
2024-10-23 22:21:44 -0300

    Correção de bugs de telas de login e nav bar

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,13 +12,13 @@ const usuario = "Daniel"; // User's name
 export default function Home() {
   return (
     <>
-    <Head>
-      <title>Resist</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-      <section className="flex flex-row justify-start h-screen bg-gradient-to-tr from-cinza-secundario to-cinza-principal w-full">
+      <Head>
+        <title>Resist</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <section className="container-principal">
         {/* Left Navigation Bar */}
-        <nav className="nav-container">
+        <nav>
           <NavBar />
         </nav>
 
@@ -35,14 +35,20 @@ export default function Home() {
             </div>
           </div>
 
-          <section className="flex flex-col lg:grid lg:grid-cols-2 gap-10 lg:gap-0">
+          <section className="home-container">
             {/* Left Dashboard Section */}
-            <div className="grid grid-flow-row px-2 lg:px-10 gap-10 lg:gap-1">
+            <div className="dashLeft-container">
+              
               {/* Welcome Section for Desktop */}
-              <WelcomeSection usuario={usuario} />
-
+              <div className="welcomeArea">
+                <p className="text-4xl text-white">Olá, {usuario}</p>
+                <p className="text-2xl text-white">
+                  Bem-vindo de volta ao seu dashboard.
+                </p>
+              </div>
+              
               {/* Graph Overview Section */}
-              {/* <GraphOverview mesData={mesData} /> */}
+              <GraphOverview/>
 
               {/* Total Lockdowns Section */}
               <TotalLockdowns />
@@ -64,155 +70,143 @@ export default function Home() {
 }
 
 // Graficos
-// const GraphOverview = ({ mesData }) => {
-//   const chartRef = useRef(null);
+const GraphOverview = ({ mesData }) => {
+  const chartRef = useRef(null);
 
-//   useEffect(() => {
-//     const ctx = chartRef.current.getContext("2d");
-//     const desktopData = mesData.map((data) => data.desktop);
-//     const mobileData = mesData.map((data) => data.mobile);
-//     const labels = mesData.map((data) => data.mes);
+  useEffect(() => {
+    const ctx = chartRef.current.getContext("2d");
+    const desktopData = mesData.map((data) => data.desktop);
+    const mobileData = mesData.map((data) => data.mobile);
+    const labels = mesData.map((data) => data.mes);
 
-//     new Chart(ctx, {
-//       type: "bar",
-//       data: {
-//         labels: labels,
-//         datasets: [
-//           {
-//             label: "Desktop",
-//             data: desktopData,
-//             backgroundColor: "#AFC3FF",
-//             borderWidth: 1,
-//           },
-//           {
-//             label: "Disp. Móveis",
-//             data: mobileData,
-//             backgroundColor: "#2D62FF",
-//             borderWidth: 1,
-//           },
-//         ],
-//       },
-//       options: {
-//         responsive: true,
-//         scales: {
-//           y: {
-//             beginAtZero: true,
-//             title: {
-//               display: true,
-//               text: "Valor",
-//             },
-//           },
-//           x: {
-//             title: {
-//               display: true,
-//               text: "Mês",
-//             },
-//           },
-//         },
-//         plugins: {
-//           legend: {
-//             display: true,
-//             position: "top",
-//           },
-//           tooltip: {
-//             enabled: true,
-//           },
-//         },
-//         animation: {
-//           duration: 1000,
-//           easing: "easeInOutQuad",
-//         },
-//       },
-//     });
-//   }, [mesData]);
+    new Chart(ctx, {
+      type: "bar",
+      data: {
+        labels: labels,
+        datasets: [
+          {
+            label: "Desktop",
+            data: desktopData,
+            backgroundColor: "#AFC3FF",
+            borderWidth: 1,
+          },
+          {
+            label: "Disp. Móveis",
+            data: mobileData,
+            backgroundColor: "#2D62FF",
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        scales: {
+          y: {
+            beginAtZero: true,
+            title: {
+              display: true,
+              text: "Valor",
+            },
+          },
+          x: {
+            title: {
+              display: true,
+              text: "Mês",
+            },
+          },
+        },
+        plugins: {
+          legend: {
+            display: true,
+            position: "top",
+          },
+          tooltip: {
+            enabled: true,
+          },
+        },
+        animation: {
+          duration: 1000,
+          easing: "easeInOutQuad",
+        },
+      },
+    });
+  }, [mesData]);
 
-//   return (
-//     <div className="flex flex-col">
-//       <h3 className="text-azul-text text-base">Visão Geral</h3>
-//       <div className="flex flex-row p-3">
-//         <canvas
-//           ref={chartRef}
-//           className="w-fit max-w-4xl h-full max-h-min rounded-s-xl bg-white"
-//         ></canvas>
-//         <div className="flex flex-col w-fit bg-cinza rounded-e-xl">
-//           {mesData.map((mes) => (
-//             <div
-//               key={mes.mes}
-//               className="grid grid-cols-4 p-2 gap-2 text-azul-text content-center justify-between self-stretch h-full px-5"
-//             >
-//               <div className="text-start">
-//                 <p className="text-base">{mes.mes}</p>
-//               </div>
-//               <div className="text-end grid grid-cols-2 justify-end">
-//                 <div className="flex flex-row justify-end">
-//                   <img
-//                     className="size-4"
-//                     src="/icons/desktopmarker.svg"
-//                     alt="Desktop marker"
-//                   />
-//                 </div>
-//                 <div className="flex flex-row justify-end">
-//                   <p className="text-base">{mes.desktop}</p>
-//                 </div>
-//               </div>
-//               <div className="text-end grid grid-cols-2 justify-end">
-//                 <div className="flex flex-row justify-end">
-//                   <img
-//                     className="size-4"
-//                     src="/icons/mobilemarker.svg"
-//                     alt="Mobile marker"
-//                   />
-//                 </div>
-//                 <div className="flex flex-row justify-end">
-//                   <p className="text-base">{mes.mobile}</p>
-//                 </div>
-//               </div>
-//               <div
-//                 className={`text-center rounded-md justify-center grid grid-cols-2 text-white ${
-//                   mes.percent > 0
-//                     ? "bg-red-status"
-//                     : mes.percent < 0
-//                     ? "bg-green-500"
-//                     : "bg-azul-principal"
-//                 }`}
-//               >
-//                 <div>
-//                   <p className="text-base">{mes.percent}%</p>
-//                 </div>
-//                 <div className="flex flex-row justify-center items-center">
-//                   <img
-//                     src="/icons/arrowWhite.svg"
-//                     className={`size-4 ${
-//                       mes.percent < 0
-//                         ? "rotate-180"
-//                         : mes.percent === 0
-//                         ? "-rotate-90"
-//                         : ""
-//                     }`}
-//                     alt="Arrow"
-//                   />
-//                 </div>
-//               </div>
-//             </div>
-//           ))}
-//           <hr />
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// Component for Welcome Section
-function WelcomeSection({ usuario }) {
   return (
-    <div className="hidden md:flex flex-col text-wrap bg-gradient-to-r from-laranja-s to-laranja-e p-5 rounded-xl">
-      <p className="text-4xl text-white">Olá, {usuario}</p>
-      <p className="text-2xl text-white">
-        Bem-vindo de volta ao seu dashboard.
-      </p>
+    <div className="flex flex-col">
+      <h3 className="text-azul-text text-base">Visão Geral</h3>
+      <div className="flex flex-row p-3">
+        <canvas
+          ref={chartRef}
+          className="w-fit max-w-4xl h-full max-h-min rounded-s-xl bg-white"
+        ></canvas>
+        <div className="flex flex-col w-fit bg-cinza rounded-e-xl">
+          {mesData.map((mes) => (
+            <div
+              key={mes.mes}
+              className="grid grid-cols-4 p-2 gap-2 text-azul-text content-center justify-between self-stretch h-full px-5"
+            >
+              <div className="text-start">
+                <p className="text-base">{mes.mes}</p>
+              </div>
+              <div className="text-end grid grid-cols-2 justify-end">
+                <div className="flex flex-row justify-end">
+                  <img
+                    className="size-4"
+                    src="/icons/desktopmarker.svg"
+                    alt="Desktop marker"
+                  />
+                </div>
+                <div className="flex flex-row justify-end">
+                  <p className="text-base">{mes.desktop}</p>
+                </div>
+              </div>
+              <div className="text-end grid grid-cols-2 justify-end">
+                <div className="flex flex-row justify-end">
+                  <img
+                    className="size-4"
+                    src="/icons/mobilemarker.svg"
+                    alt="Mobile marker"
+                  />
+                </div>
+                <div className="flex flex-row justify-end">
+                  <p className="text-base">{mes.mobile}</p>
+                </div>
+              </div>
+              <div
+                className={`text-center rounded-md justify-center grid grid-cols-2 text-white ${
+                  mes.percent > 0
+                    ? "bg-red-status"
+                    : mes.percent < 0
+                    ? "bg-green-500"
+                    : "bg-azul-principal"
+                }`}
+              >
+                <div>
+                  <p className="text-base">{mes.percent}%</p>
+                </div>
+                <div className="flex flex-row justify-center items-center">
+                  <img
+                    src="/icons/arrowWhite.svg"
+                    className={`size-4 ${
+                      mes.percent < 0
+                        ? "rotate-180"
+                        : mes.percent === 0
+                        ? "-rotate-90"
+                        : ""
+                    }`}
+                    alt="Arrow"
+                  />
+                </div>
+              </div>
+            </div>
+          ))}
+          <hr />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 // Component for Total Lockdowns
 function TotalLockdowns() {
