ff23bd48dffb4052c57cb4add7a41d7d777e9054
Daniel Augusto Mandira <daniel.mandira@hotmail.com>
2024-10-25 18:53:11 -0300

    inicio do carrosel de incidencia

--- src/pages/components/Incidencia.js
+++ src/pages/components/Incidencia.js
@@ -1,12 +1,50 @@
 import Chart from "chart.js/auto";
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 const Incidencia = () => {
+  data=[{index: 0, value:}]
   const chartRef = useRef(null);
   useEffect(() => {
     const ctx = document.getElementById("LaboratorioTeste").getContext("2d");
     if (chartRef.current) {
       chartRef.current.destroy();
     }
+
+    const [currentIndex, setCurrentIndex] = useState(0)
+    const [numItemsShow, setNumItemsShow] = useState(4)
+    const carouselRef = useRef(null);
+    const startXRef = useRef(0);
+
+    const handleSlideChange = (direction) =>{
+      if(direction === "prev"){
+        setCurrentIndex((prevIndex) =>{
+          prevIndex === 0 ? data.lenght -1 : prevIndex - 1
+        })
+      } else{
+        setCurrentIndex((prevIndex) =>(prevIndex +1) % data.lenght)
+      }
+    }
+
+    const handleDragStart = (event) => {
+      startXRef.current = event.clientX || event.touches[0].clientX
+      document.addEventListener("mouseup", handleDragEnd)
+      document.addEventListener("touchend", handleDragEnd)
+    }
+
+    const handleDragEnd = (event) => {
+      document.removeEventListener("mouseup", handleDragEnd)
+      document.removeEventListener("touchend", handleDragEnd)
+
+      const currentX = event.ClientX || event.changeTouches[0].clientX
+      const deltaX = currentX - startXRef.current
+      const threshold = carouselRef.current.offsetWith * 0.2
+
+      if (deltaX > threshold){
+        handleSlideChange("prev")
+      }else if (deltaX < -threshold){
+        handleSlideChange("next")
+      }
+    }
+
 
     chartRef.current = new Chart(ctx, {
       type: "doughnut",
@@ -42,9 +80,14 @@ const Incidencia = () => {
         </p>
         <div className="flex flex-row gap-4">
           {/* Graficos */}
-          <div class="flex flex-row gap-3">
+          <div class="flex flex-row gap-3"
+          ref={carouselRef}
+          onMouseDown={handleDragStart}
+          onMouseUp={handleDragEnd}
+          onTouchStart={handleDragStart}
+          onTouchEnd={handleDragEnd}>
             {/* Lab */}
-            <div class="bg-azul-principal rounded-xl h-max">
+            {[...data, ...data, ...data].slice(currentIndex, currentIndex + numItemsShow).map((dados, index)=>(            <div class="bg-azul-principal rounded-xl h-max" >
               <p class="text-white text-base p-3 text-center">
                 Laboratorio Teste
               </p>
@@ -54,6 +97,7 @@ const Incidencia = () => {
                 <p class="text-azul-text text-xl absolute z-10">50%</p>
               </div>
             </div>
+            ))}
           </div>
         </div>
       </div>
