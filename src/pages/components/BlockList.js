9e452f9e8c850cbf2277d416c43a09a691a21a0c
Daniel Augusto Mandira <daniel.mandira@hotmail.com>
2024-11-14 22:13:53 -0300

    Correção e aplicação de tela de bloqueios

--- src/pages/components/BlockList.js
+++ src/pages/components/BlockList.js
@@ -1,92 +1,63 @@
+import { useState, useEffect } from "react";
+import url from "../services/url";
+import axios from "axios";
 const BlockList = () => {
-  const bloqueios = [
-    {
-      url: "https://example.com",
-      dataHora: "2022-01-01",
-    },
-    {
-      url: "https://example.com",
-      dataHora: "2022-01-01",
-    },
-    {
-      url: "https://example.com",
-      dataHora: "2022-01-01",
-    },
-    {
-      url: "https://example.com",
-      dataHora: "2022-01-01",
-    },
-    {
-      url: "https://example.com",
-      dataHora: "2022-01-01",
-    },
-    {
-      url: "https://example.com",
-      dataHora: "2022-01-01",
-    },
-    {
-      url: "https://example.com",
-      dataHora: "2022-01-01",
-    },
-    {
-      url: "https://example.com",
-      dataHora: "2022-01-01",
-    },
-    {
-      url: "https://example.com",
-      dataHora: "2022-01-01",
-    },
-    {
-      url: "https://example.com",
-      dataHora: "2022-01-01",
-    },
-  ];
+  const [bloqueios, setBloqueios] = useState([]);
+
+  const fetchBloqueios = async()=>{
+    try{
+      const response = await axios.get(`${url}/bloqueios`)
+      setBloqueios(response.data)
+
+    }catch(error){
+      console.error("Error fetching bloqueios:", error);
+    }
+  }
+
+  useEffect(()=>{
+    fetchBloqueios()
+  }, [])
   return (
-    <div class="Flex flex-col">
-      <div class="flex flex-row justify-between pb-1 ">
-        <h3 class="text-azul-text text-lg font-bold">
+    <div className="Flex flex-col">
+      <div className="flex flex-row justify-between pb-1 ">
+        <h3 className="text-azul-text text-lg font-bold">
           Editar bloqueios de URL
         </h3>
-        <div class="flex flex-row justify-center items-center gap-2">
+        <div className="flex flex-row justify-center items-center gap-2">
           <button
             onclick="ordenarURLs()"
-            class="rounded  text-white w-max px-3 py-1 bg-azul-buttom"
+            className="rounded  text-white  px-3 py-1 bg-azul-buttom"
           >
             Ordenar URL's
           </button>
-          <button class="rounded  text-white w-max p-1 px-2 bg-azul-buttom">
+          <button className="rounded  text-white  p-1 px-2 bg-azul-buttom">
             <img src="/icons/edit.svg" />
           </button>
-          <button class="rounded  text-white w-max p-1 px-2 bg-azul-buttom">
+          <button className="rounded  text-white  p-1 px-2 bg-azul-buttom">
             <img src="/icons/delete.svg" />
           </button>
         </div>
       </div>
-      <div class="bg-white rounded-xl w-max p-5 gap-5 overflow-y-scroll max-h-96">
-        <table class="table-auto w-full border-separate border-spacing-2 ">
-          <thead>
-            <tr class="text-azul-text">
-              <th>Tipo Disp.</th>
-              <th>URL Bloqueado</th>
-              <th>Data</th>
-              <th>Tipo Bloq.</th>
-              <th>Selec./Editar</th>
+      <div className="bg-white rounded-xl p-2 gap-5 overflow-y-scroll max-h-96">
+        <table className="table-auto w-full ">
+          <thead className="border-b border-black" >
+            <tr className="text-azul-text ">
+              <th className="border-b border-black">URL Bloqueado</th>
+              <th className="border-b border-black">Data</th>
+              <th className="border-b border-black">Tipo Bloq.</th>
+              <th className="p-2 border-b border-black">Selec./Editar</th>
             </tr>
           </thead>
-          <tbody class="text-azul-text">
+          <tbody className="text-azul-text">
             {bloqueios.map((bloq) => (
-              <tr class="bloqueio-row border-b justify-self-center  ">
-                <td class=" flex flex-col items-center justify-center">
-                  <img src="/icons/iconDesktop.svg" />
-                </td>
-                <td class="url font-bold">{bloq.url}</td>
-                <td>{bloq.dataHora}</td>
-                <td class="font-bold">Automático</td>
-                <td class="grid grid-cols-2">
-                  <input class="cursor-pointer" type="checkbox" name="select" />
-                  <select>
-                    <option class="rotate-180" value="">
-                      ^
+              <tr className="  justify-self-center  border-black">
+                <td className="p-2 font-bold border-b border-black">{bloq.urlWeb}</td>
+                <td className="p-2 text-nowrap  border-b border-black">{bloq.dataHora}</td>
+                <td className="p-2 font-bold border-b border-black">{bloq.tipoInsercao}</td>
+                <td className="grid grid-cols-2 p-2 border-b border-black">
+                  <input className="cursor-pointer" type="checkbox" name="select" />
+                  <select className="w-8">
+                    <option className="" value="">
                     </option>
                     <option value="details">Detalhes</option>
                   </select>
