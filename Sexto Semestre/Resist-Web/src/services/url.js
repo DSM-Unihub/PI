e3079a8961efb3e491c086b141309c1b74e25754
Bruno Freitas <brunofreitasd11@gmail.com>
2026-05-20 13:26:15 -0300

    Corrige endereço da api

--- Sexto Semestre/Resist-Web/src/services/url.js
+++ Sexto Semestre/Resist-Web/src/services/url.js
@@ -1,5 +1,5 @@
 // const url = "https://resist-api-production.up.railway.app/api"
 //const url = "http://192.168.12.1:4000/api"
- const url = "https://pi-production-2086.up.railway.app/api"
+const url = process.env.NEXT_PUBLIC_API_URL
 
 export default url;
